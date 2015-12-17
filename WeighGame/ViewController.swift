8aabb8d2b123906003af5e374ddfa5ab1089502e
나김준우 <Joonwoo@imac.unet.ca>
2015-12-16 22:15:52 -0800

    Initial Commit

---
+++ WeighGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeighGame
+//
+//  Created by 김준우 on 2015. 12. 16..
+//  Copyright © 2015년 김준우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
